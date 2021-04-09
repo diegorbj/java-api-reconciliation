8cbedef436ecab3c8c9c399460c4aad078042355
Diego Barros <diegorbj@outlook.com>
2021-04-09 17:22:11 -0300

    Implementing query example at CardType class

--- src/main/java/com/diegorbj/reconciliation/repositories/CardTypeRepository.java
+++ src/main/java/com/diegorbj/reconciliation/repositories/CardTypeRepository.java
@@ -2,11 +2,17 @@ package com.diegorbj.reconciliation.repositories;
 
 import com.diegorbj.reconciliation.domain.CardType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface CardTypeRepository extends JpaRepository<CardType, Long> {
 
     List<CardType> findByNameIgnoreCase(String name);
+
+    @Query(value = "SELECT * FROM tb_card_type ct WHERE ct.card_type_id in :card_type_ids", nativeQuery = true)
+    List<CardType> getByCardTypes(@Param("card_type_ids") Collection<Long> cardTypeIds);
 
 }
